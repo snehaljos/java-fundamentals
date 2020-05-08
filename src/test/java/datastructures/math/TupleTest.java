ae33a6f104702a33906c97666ec36c7b1adc07a2
Lorenz Nickel <lorenz.nickel@gmx.net>
2020-05-08 23:02:16 +0200

    Improve test-coverage for Tuple

--- src/test/java/datastructures/math/TupleTest.java
+++ src/test/java/datastructures/math/TupleTest.java
@@ -8,12 +8,31 @@ import junit.framework.TestCase;
 public class TupleTest extends TestCase {
 
   public void testImmutability() {
-    Tuple<Integer> tupleA = new Tuple<>(1, 2);
+    final Tuple<Integer> tupleA = new Tuple<>(1, 2);
     assertEquals(tupleA.getA(), Integer.valueOf(1));
     assertEquals(tupleA.getB(), Integer.valueOf(2));
-    Tuple<Integer> tupleB = tupleA.withA(3);
+    final Tuple<Integer> tupleB = tupleA.withA(3);
     assertEquals(tupleA.getA(), Integer.valueOf(1));
     assertEquals(tupleB.getA(), Integer.valueOf(3));
+  }
+
+  public void testEquals() {
+    final Tuple<Integer> tuple = new Tuple<>(1,2);
+    assertEquals(tuple, new Tuple<>(1, 2));
+    assertNotSame(null, tuple);
+    assertNotSame(tuple, null);
+    assertNotSame(1, tuple);
+    assertNotSame(tuple, 1);
+    assertNotSame(tuple, new Tuple<>(2, 1));
+  }
+
+  public void testHashCode() {
+    assertEquals(new Tuple<>("a", "b"), new Tuple<>("a", "b"));
+    assertNotSame(new Tuple<>("a", "b"), new Tuple<>("b", "a"));
+  }
+
+  public void testToString() {
+    assertEquals(new Tuple<>(2.0f, 1.0f).toString(), "Tuple{a=2.0, b=1.0}");
   }
 
 }

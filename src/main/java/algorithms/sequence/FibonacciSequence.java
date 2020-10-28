eaa0c8822400a749f15ca38e43e177f3455207eb
José Cunha <josefdcunha97@gmail.com>
2020-10-28 16:02:56 +0000

    Add Fibonacci Iterative implementation

--- src/main/java/algorithms/sequence/FibonacciSequence.java
+++ src/main/java/algorithms/sequence/FibonacciSequence.java
@@ -13,13 +13,34 @@ public class FibonacciSequence {
    * @param n the position in Fibonacci Sequence
    * @return the number in postion n, if n is 0 or negative will return -1
    */
-  public static int fibonacci(int n) {
+  public static int recursive(int n) {
     if (n < 0) {
       return -1;
     }
-    if (n == 0 || n == 1) {
+    if (n <= 1) {
       return n;
     }
-    return fibonacci(n - 1) + fibonacci(n - 2);
+    return recursive(n - 1) + recursive(n - 2);
+  }
+
+  /**
+   * Return the number in n position in Fibonacci Sequence (iterative)
+   * @param n the position in Fibonacci Sequence
+   * @return the number in postion n, if n is 0 or negative will return -1
+   */
+  public static int iterative(int n) {
+    int preN = 0, currN = 1, auxN;
+    if (n < 0) {
+      return -1;
+    }
+    if (n <= 1) {
+      return n;
+    }
+    for (int i = 1; i < n; i++) {
+      auxN = preN;
+      preN = currN;
+      currN = auxN + preN;
+    }
+    return currN;
   }
 }
--- src/test/java/algorithms/sequence/FibonacciSequenceTest.java
+++ src/test/java/algorithms/sequence/FibonacciSequenceTest.java
@@ -7,20 +7,37 @@ import junit.framework.TestCase;
  */
 public class FibonacciSequenceTest extends TestCase {
 
-  public void testInitialValues() {
-    assertEquals(0, FibonacciSequence.fibonacci(0));
-    assertEquals(1, FibonacciSequence.fibonacci(1));
+  public void testInitialValuesRecursive() {
+    assertEquals(0, FibonacciSequence.recursive(0));
+    assertEquals(1, FibonacciSequence.recursive(1));
   }
 
-  public void testInvalidValues() {
-    assertEquals(-1 , FibonacciSequence.fibonacci(-1));
-    assertEquals(-1, FibonacciSequence.fibonacci(-3));
+  public void testInvalidValuesRecursive() {
+    assertEquals(-1 , FibonacciSequence.recursive(-1));
+    assertEquals(-1, FibonacciSequence.recursive(-3));
   }
 
-  public void testSequence() {
-    assertEquals(1, FibonacciSequence.fibonacci(2));
-    assertEquals(3, FibonacciSequence.fibonacci(4));
-    assertEquals(8, FibonacciSequence.fibonacci(6));
-    assertEquals(21, FibonacciSequence.fibonacci(8));
+  public void testSequenceRecursive() {
+    assertEquals(1, FibonacciSequence.recursive(2));
+    assertEquals(3, FibonacciSequence.recursive(4));
+    assertEquals(8, FibonacciSequence.recursive(6));
+    assertEquals(21, FibonacciSequence.recursive(8));
+  }
+
+  public void testInitialValuesIterative() {
+    assertEquals(0, FibonacciSequence.iterative(0));
+    assertEquals(1, FibonacciSequence.iterative(1));
+  }
+
+  public void testInvalidValuesIterative() {
+    assertEquals(-1 , FibonacciSequence.iterative(-1));
+    assertEquals(-1, FibonacciSequence.iterative(-3));
+  }
+
+  public void testSequenceIterative() {
+    assertEquals(1, FibonacciSequence.iterative(2));
+    assertEquals(3, FibonacciSequence.iterative(4));
+    assertEquals(8, FibonacciSequence.iterative(6));
+    assertEquals(21, FibonacciSequence.iterative(8));
   }
 }

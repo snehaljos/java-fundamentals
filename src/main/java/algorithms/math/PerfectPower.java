e0e7d5b0b398c358d603832c6fe52000f09ceb17
Lorenz Nickel <lorenz.nickel@gmx.net>
2021-10-15 20:44:33 +0200

    Fix javadoc error

--- src/main/java/algorithms/math/PerfectPower.java
+++ src/main/java/algorithms/math/PerfectPower.java
@@ -5,14 +5,14 @@ package algorithms.math;
  * and k is the exponent.
  *
  * @author nujovich
- * @see <a ref="https://en.wikipedia.org/wiki/Perfect_power"></a>
+ * @see <a href="https://en.wikipedia.org/wiki/Perfect_power"></a>
  */
 public class PerfectPower {
 
   /**
    * @param n int, natural number
-   * @return and array where the first element is the root and the second element is the exponent.
-   * @apiNote if n has more than one tuple, the algorithm returns only the first one found.
+   * @return an array where the first element is the root and the second element is the exponent. If
+   * n has more than one tuple, the algorithm returns only the first one found.
    */
   public static int[] isPerfectPower(int n) {
     for (int i = 2; ; i++) {
